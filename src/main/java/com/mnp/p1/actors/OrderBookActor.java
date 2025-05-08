42d334ff56a61d5caa83ec5c4b6fa6d695af1e20
illfixit <66363651+illfixit@users.noreply.github.com>
2025-05-08 20:07:28 +0200

    improved basic structure

---
+++ src/main/java/com/mnp/p1/actors/OrderBookActor.java
@@ -0,0 +1,12 @@
+package com.mnp.p1.actors;
+
+public class OrderBookActor {
+
+    /*
+     * • Auftragsbuch (OrderBook)
+     * – Neuer Auftrag: Ein neuer Auftrag wird in das Auftragsbuch übernommen, dies kann einfach eine Nummer sein.
+     * – Auftrag zuweisen: Der älteste nicht bearbeitete Auftrag wird der nächsten freien Produktionsstraße
+     *  zugewiesen. Sollte keine vorliegen, wird 10 Sekunden gewartet und ein neuer Zuweisungsversuch unternommen.
+     */
+
+}
---
+++ src/main/java/com/mnp/p1/actors/ProductionLineActor.java
@@ -0,0 +1,13 @@
+package com.mnp.p1.actors;
+
+public class ProductionLineActor {
+
+    /*
+     * • Produktionsstraße (ProductionLine)
+     * – Produziere Karosserie: Die Karosserie wird durch den zugewiesenen Arbeiter in [5,10] Sekunden erstellt.
+     * – Verbaue Spezialwünsche: Nachdem die Karosserie erstellt wurde,
+     *   werden die 2 Spezialwünsche aus dem Lager geholt und eingebaut.
+     * – Fertigstellung: Die Straße und die / der Arbeiter*in werden wieder freigegeben.
+     */
+
+}
